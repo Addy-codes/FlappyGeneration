a1e06774290f0f9b357cd7546b18bd320ee1cc67
Addy-codes <adeeb9839@gmail.com>
2023-12-18 14:21:04 +0530

    pages added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
 import openai
 from config import(
@@ -6,9 +6,12 @@ from config import(
     CLIPDROP_API_KEY,
     OPENAI_API_KEY,
     NETLIFY_ACCESS_TOKEN,
+    SECRET_KEY,
 )
 
 app = Flask(__name__)
+
+app.secret_key = SECRET_KEY
 
 # MongoDB connection
 client = MongoClient("mongodb_uri")  # replace with your MongoDB URI
@@ -27,7 +30,10 @@ def login():
         phone = request.form.get('phone')
         
         # Save to MongoDB
-        users_collection.insert_one({'name': name, 'email': email, 'phone': phone})
+        # users_collection.insert_one({'name': name, 'email': email, 'phone': phone})
+
+        # Save user info in session
+        session['user_info'] = {'name': name, 'email': email, 'phone': phone}
         
         return redirect(url_for('theme'))
 
@@ -40,12 +46,32 @@ def theme():
             theme = request.form.get('theme')
             # Process the theme here
             processed_theme = process_theme(theme)  # Implement this function
-            return render_template('theme.html', theme=processed_theme)
-        
-        elif 'proceed' in request.form:
-            return redirect(url_for('assets'))
+
+            # Parse and save processed theme in session
+            parsed_theme = parse_processed_theme(processed_theme)
+            session['processed_theme'] = parsed_theme
+
+            return render_template('modifytheme.html', processed_theme=parsed_theme)
 
     return render_template('theme.html')
+
+@app.route('/modifytheme', methods=['GET', 'POST'])
+def modify_theme():
+    if request.method == 'POST':
+        main_character = request.form.get('main_character')
+        game_background = request.form.get('game_background')
+        top_obstacle = request.form.get('top_obstacle')
+        bottom_obstacle = request.form.get('bottom_obstacle')
+
+        generate_assets()
+        # Process this data or store it as needed
+        # For example, redirecting to another page with this data
+        return redirect(url_for('assets'))  # Replace 'next_page' with your next page
+
+    # Retrieve parsed processed theme from session
+    processed_theme = session.get('processed_theme', {})
+    return render_template('modifytheme.html', processed_theme=processed_theme)
+
 
 @app.route('/assets')
 def assets():
@@ -70,6 +96,25 @@ def process_theme(theme):
     )
 
     return response.choices[0].text.strip()
+
+def parse_processed_theme(processed_theme_str):
+    theme_dict = {}
+    lines = processed_theme_str.split('\n')
+    for line in lines:
+        if 'Obstacle 1:' in line:
+            theme_dict['top_obstacle'] = line.split(': ')[1]
+        elif 'Obstacle 2:' in line:
+            theme_dict['bottom_obstacle'] = line.split(': ')[1]
+        elif 'Main Character:' in line:
+            theme_dict['main_character'] = line.split(': ')[1]
+        elif 'Background Image:' in line:
+            theme_dict['game_background'] = line.split(': ')[1]
+
+    return theme_dict
+
+def generate_assets():
+    pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -4,9 +4,11 @@ import json
 BASE_PATH = os.getcwd()
 
 # Loading the config.json file
-config_path = os.path.join(BASE_PATH, 'key', 'config.json')
+config_path = os.path.join(BASE_PATH, 'keys', 'config.json')
 with open(config_path) as f:
     config = json.load(f)
+
+SECRET_KEY = "Addy-Codes"
 
 # Assigning the keys to variables
 STABILITY_API_KEY = config.get('stability_api_key')
